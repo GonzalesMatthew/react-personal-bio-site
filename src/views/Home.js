10586fc8e9ab685a7d7b9ea9dd99e703846c6d61
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-05-22 19:55:19 -0500

    update bio to Home component

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Home() {
   return (
-    <h1>Welcome</h1>
+    <>
+      <div>
+        <h1>Welcome</h1>
+      </div>
+      <div>
+          Hi, my name is Matthew and I am a creative individual that loves
+          creative people and activities. Some of my favorite hobbies include
+          composing, hiking, travel, and meeting new people. I have an undergrad
+          degree from UCSDs math department and have been doing analytics for
+          some time. When the most enjoyable part is dashboard development and
+          building the data pipelines thats when you know you should become a
+          full-time dev. Outside of development I enjoy MMA, F1 racing, cooking
+          new recipes, and studying financial markets.
+      </div>
+    </>
   );
 }
 
