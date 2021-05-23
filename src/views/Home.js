5a68cf046412985f17fed8c772c833133d317a32
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-05-22 20:07:43 -0500

    copied over about-me and contact-info from prior project

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,16 +5,9 @@ function Home() {
     <>
       <div>
         <h1>Welcome</h1>
-      </div>
-      <div>
-          Hi, my name is Matthew and I am a creative individual that loves
-          creative people and activities. Some of my favorite hobbies include
-          composing, hiking, travel, and meeting new people. I have an undergrad
-          degree from UCSDs math department and have been doing analytics for
-          some time. When the most enjoyable part is dashboard development and
-          building the data pipelines thats when you know you should become a
-          full-time dev. Outside of development I enjoy MMA, F1 racing, cooking
-          new recipes, and studying financial markets.
+        <p>
+          Hello, my name is Matthew and I consider myself to be a creative indidual.I pursued development to constantly work creatively and to spend more time with people who enjoy doing the same. I graduated from the University of California, San Diego, and have been in analytics for a number of years before switching to development. Some of my favorite hobbies include composing music, hiking, traveling, and making new friends. I also enjoy watching mixed martial arts and F1 racing, cooking, studying financial markets, and decorating my home.
+        </p>
       </div>
     </>
   );
