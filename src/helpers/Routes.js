87496debe7313cc94efcaa322e1c92aa1d924d2e
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-05-22 19:21:45 -0500

    created navBar links and started making privateRoutes

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -2,31 +2,33 @@ import React from 'react';
 import {
   Route,
   Switch,
-  // Redirect
+  Redirect
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Home from '../views/Home';
 import NotFound from '../views/NotFound';
+import Projects from '../views/Projects';
+import Technology from '../views/Technologies';
+import Contact from '../views/Contact';
 
 // The PrivateRoute function is creating a private route and returing the specified route based on the props
 // We specify the specific props we want to use in the routeChecker and pass the rest with the spread
 
-// const PrivateRoute = ({ component: Component, user, ...rest }) => {
-
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
 // when we call this function in the return, it is looking for an argument. `props` here is taco.
 
-//   const routeChecker = (taco) => (user
-//     ? (<Component {...taco} user={user} />)
-//     : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
-//     // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
-//   // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
-//   return <Route {...rest} render={(props) => routeChecker(props)} />;
-// };
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} user={user} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+    // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
+  // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
-// PrivateRoute.propTypes = {
-//   component: PropTypes.func,
-//   user: PropTypes.any
-// };
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
 
 export default function Routes() {
 // export default function Routes({ user }) {
@@ -34,9 +36,10 @@ export default function Routes() {
     <div>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/projects' component={Home} />
-        <Route exact path='/technology' component={Home} />
-        <Route exact path='/contact' component={Home} />
+        <Route exact path='/portfolio' component={Projects} />
+        <Route exact path='/technology' component={Technology} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/logon' component={Home} />
         {/* <PrivateRoute
           exact
           path='/about-edit'
