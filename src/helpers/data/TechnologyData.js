aaf9cf90ad1045b470b78678e88b0cb2930668a0
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-05-24 21:25:48 -0500

    creating APIs for tech and project CRUD

--- src/helpers/data/TechnologyData.js
+++ src/helpers/data/TechnologyData.js
@@ -9,4 +9,24 @@ const getTechnology = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default getTechnology;
+const createTechnology = (techObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/technology.json`, techObj)
+    .then(getTechnology)
+    .catch((error) => reject(error));
+});
+
+const updateTechnology = (firebaseKey, techObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/technology/${firebaseKey}`, techObj)
+    .then(getTechnology())
+    .catch((error) => reject(error));
+});
+
+const deleteTechnology = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/technology/${firebaseKey}`)
+    .then(() => getTechnology().then)
+    .catch((error) => reject(error));
+})
+
+export default {
+  getTechnology, createTechnology, updateTechnology, deleteTechnology
+};
